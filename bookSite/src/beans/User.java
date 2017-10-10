e4ca8643de1bb1ca100f580db4be32c578812cc0
wipro-ivan <32336786+wipro-ivan@users.noreply.github.com>
2017-10-10 13:54:39 +0100

    Uploaded updated User bean

--- bookSite/src/beans/User.java
+++ bookSite/src/beans/User.java
@@ -1,5 +1,127 @@
 package beans;
 
 public class User {
-
+    String employeeID;
+    String firstName;
+    String lastName;
+    String DOB;
+    String emailAddr;
+    String password;
+    String addressLine1;
+    String addressLine2;
+    String country;
+    String county;
+    String postCode;
+    String contactNumber;
+    String cardNumber;
+    String cardName;
+    String cardExpDate;
+    String securityLevel;
+   
+    public String getEmployeeID() {
+          return employeeID;
+    }
+    public void setEmployeeID(String employeeID) {
+          this.employeeID = employeeID;
+    }
+    public String getFirstName() {
+          return firstName;
+    }
+    public void setFirstName(String firstName) {
+          this.firstName = firstName;
+    }
+    public String getLastName() {
+          return lastName;
+    }
+    public void setLastName(String lastName) {
+          this.lastName = lastName;
+    }
+    public String getDOB() {
+          return DOB;
+    }
+    public void setDOB(String dOB) {
+          DOB = dOB;
+    }
+    public String getEmailAddr() {
+          return emailAddr;
+    }
+    public void setEmailAddr(String emailAddr) {
+          this.emailAddr = emailAddr;
+    }
+    public String getPassword() {
+          return password;
+    }
+    public void setPassword(String password) {
+          this.password = password;
+    }
+    public String getAddressLine1() {
+          return addressLine1;
+    }
+    public void setAddressLine1(String addressLine1) {
+          this.addressLine1 = addressLine1;
+    }
+    public String getAddressLine2() {
+          return addressLine2;
+    }
+    public void setAddressLine2(String addressLine2) {
+          this.addressLine2 = addressLine2;
+    }
+    public String getCountry() {
+          return country;
+    }
+    public void setCountry(String country) {
+          this.country = country;
+    }
+    public String getCounty() {
+          return county;
+    }
+    public void setCounty(String county) {
+          this.county = county;
+    }
+    public String getPostCode() {
+          return postCode;
+    }
+    public void setPostCode(String postCode) {
+          this.postCode = postCode;
+    }
+    public String getContactNumber() {
+          return contactNumber;
+    }
+    public void setContactNumber(String contactNumber) {
+          this.contactNumber = contactNumber;
+    }
+    public String getCardNumber() {
+          return cardNumber;
+    }
+    public void setCardNumber(String cardNumber) {
+          this.cardNumber = cardNumber;
+    }
+    public String getCardName() {
+          return cardName;
+    }
+    public void setCardName(String cardName) {
+          this.cardName = cardName;
+    }
+    public String getCardExpDate() {
+          return cardExpDate;
+    }
+    public void setCardExpDate(String cardExpDate) {
+          this.cardExpDate = cardExpDate;
+    }
+    public String getSecurityLevel() {
+          return securityLevel;
+    }
+    public void setSecurityLevel(String securityLevel) {
+          this.securityLevel = securityLevel;
+    }
+    
+    User (){
+          
+    }
+    
+    
+    
+    
+    
+    
 }
